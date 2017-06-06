94a65fbe5fd72b69b9cac7d05df9ea1615339c60
Jin Chen <jinchen93@gmail.com>
2017-06-05 17:04:21 -0700

    Add basic app component layout

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="sidebar">
+    <h1>Synapsis</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/components/Visualization.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="visualization">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'visualization',
+  data() {
+    return {};
+  }
+};
+</script>
