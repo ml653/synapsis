a65b9eec3cac08bd39c1c800122fcaee352b2118
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-06 09:17:50 -0700

    Rename data_structure.json to .js file. Loaded js file into data

--- src/components/visualization/Visualization.vue
+++ src/components/visualization/Visualization.vue
@@ -7,33 +7,15 @@
 </template>
 
 <script>
+import pojo from '../../../data_structure.js';
 export default {
   name: 'visualization',
   data() {
     return {
-      convnet: null
+      convnet: pojo
     };
   },
-  methods: {
-    loadJSON: function(callback) {   
-
-        var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType("application/json");
-        xobj.open('GET', '../../../build/data_structure.json', true); // Replace 'my_data' with the path to your file
-        xobj.onreadystatechange = function () {
-              if (xobj.readyState == 4 && xobj.status == "200") {
-                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-                callback(xobj.responseText);
-              }
-        };
-        xobj.send(null);  
-    },
-    getPojo: function() {
-      this.convet = loadJSON();
-    }
-  },
   mounted: function() {
-    this.getPojo()
   },
   components: {
 
