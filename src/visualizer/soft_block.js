6c3b3fae76dc16a9c902b4bd045914c913f4c067
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-09 11:47:07 -0700

    Implement SoftBlock skeleton

---
+++ src/visualizer/soft_block.js
@@ -0,0 +1,11 @@
+import Vector from './vector';
+import CanvBlock from './canv_block';
+
+class SoftBlock extends CanvBlock {
+  constructor(layer) {
+    super();
+    this.layer = layer;
+  }
+}
+
+export default GridBlock;
