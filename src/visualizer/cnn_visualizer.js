5881bf973daff3b8cb624697c5957ff4d54f87dd
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-06-09 11:28:34 -0700

    Implement skeleton for CnnVisualizer#_generateBlocks

--- src/visualizer/cnn_visualizer.js
+++ src/visualizer/cnn_visualizer.js
@@ -1,3 +1,9 @@
+import InputBlock from './input_block';
+import ConvBlock from './conv_block';
+import PoolBlock from './pool_block';
+import ConnBlock from './conn_block';
+import SoftBlock from './soft_block';
+
 class CnnVisualizer {
   constructor(canvasEl) {
     this.canvasEl = canvasEl;
@@ -5,8 +11,12 @@ class CnnVisualizer {
   }
 
   update(cnn) {
+    this.cnn = cnn;
+
     if (this.blocks.length === 0)
-      this._generateBlocks(cnn);
+      this._generateBlocks();
+
+    this._positionBlocks();
     const ctx = this.canvasEl.getContext('2d');
     ctx.clearRect(0, 0, this.width, this.height);
     ctx.fillRect(25, 25, 100, 100);
@@ -17,9 +27,36 @@ class CnnVisualizer {
     this.height = height;
     this.canvasEl.width = width;
     this.canvasEl.height = height;
+    this.update(this.cnn);
   }
 
-  _generateBlocks(cnn) {
+  _generateBlocks() {
+    console.log(this.cnn);
+
+    for (let i = 0; i < this.cnn.length; i++) {
+      let layer = this.cnn[i];
+      if (layer.type === 'softmax')
+        this.blocks.push(new SoftBlock(layer));
+      else if (layer.type === 'fc')
+        this.blocks.push(new ConnBlock(layer));
+      for(let x = 0; x < layer.blocks.length; x++) {
+        let blkInfo = layer.blocks[x];
+        this.blocks.push(this._generateBlock(layer.type, blkInfo));
+      } 
+    }
+  }
+
+  _generateBlock(type, info) {
+    if (type === 'input')
+      return new InputBlock(info);
+    if (type === 'conv')
+      return new ConvBlock(info);
+    if (type === 'pool')
+      return new PoolBlock(info);
+    return null;
+  }
+
+  _positionBlocks() {
   }
 }
 
